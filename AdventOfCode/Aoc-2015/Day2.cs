efc8cdf344c73684d68f8377af7c67be90382aa0
Mustafin Vadim <mystafin@skbkontur.ru>
2024-03-20 21:12:52 +0500

    day 2 2015

---
+++ AdventOfCode/Aoc-2015/Day2.cs
@@ -0,0 +1,26 @@
+﻿namespace AoC;
+
+public class Day2
+{
+    public static int Part1(List<string> input)
+    {
+        return input.Select(Parse)
+            .Select(x => (s1: x.l * x.w, s2: x.w * x.h, s3: x.h * x.l))
+            .Select(x => new[] { x.s1, x.s2, x.s3 }.Min() + 2 * (x.s1 + x.s2 + x.s3))
+            .Sum();
+    }
+
+    public static int Part2(List<string> input)
+    {
+        return input.Select(Parse)
+            .Select(x => (x.l * x.w * x.h, new[] { x.l, x.w, x.h }.OrderBy(y => y).Take(2).ToList()))
+            .Select(x => x.Item1 + 2 * (x.Item2[0] + x.Item2[1]))
+            .Sum();
+    }
+
+    private static (int l, int w, int h) Parse(string line)
+    {
+        var splitLine = line.Split('x');
+        return (int.Parse(splitLine[0]), int.Parse(splitLine[1]), int.Parse(splitLine[2]));
+    }
+}
--- AdventOfCode/Aoc-2015/Program.cs
+++ AdventOfCode/Aoc-2015/Program.cs
@@ -1,7 +1,7 @@
 ﻿using AoC;
 using Tools;
 
-var data = await Data.GetInput(1).ToListAsync();
+var data = await Data.GetInput(2).ToListAsync();
 
-Console.WriteLine(Day1.Part1(data.First()!));
-Console.WriteLine(Day1.Part2(data.First()!));
+Console.WriteLine(Day2.Part1(data));
+Console.WriteLine(Day2.Part2(data));
