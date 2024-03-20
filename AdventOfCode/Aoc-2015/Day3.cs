8207ead0753acecdbf2a5ddf1f8336fdd9042c71
Mustafin Vadim <mystafin@skbkontur.ru>
2024-03-20 22:28:08 +0500

    day 3 2015

---
+++ AdventOfCode/Aoc-2015/Day3.cs
@@ -0,0 +1,56 @@
+﻿namespace AoC;
+
+public class Day3
+{
+    public static int Part1(string input)
+    {
+        var location = (0, 0);
+        var set = new HashSet<(int, int)> { location };
+
+        foreach (var dir in input)
+        {
+            location = DoStep(location, dir);
+            set.Add(location);
+        }
+        
+        return set.Count;
+    }
+
+    public static int Part2(string input)
+    {
+        var santa = (0, 0);
+        var robot = (0, 0);
+        var isSanta = true;
+        var set = new HashSet<(int, int)> { santa };
+
+        foreach (var dir in input)
+        {
+            if (isSanta)
+            {
+                santa = DoStep(santa, dir);
+                set.Add(santa);
+            }
+            else
+            {
+                robot = DoStep(robot, dir);
+                set.Add(robot);
+            }
+            
+            isSanta = !isSanta;
+        }
+
+        return set.Count;
+    }
+
+    private static (int x, int y) DoStep((int x, int y) current, char direction)
+    {
+        return direction switch
+        {
+            '^' => (current.x, ++current.y),
+            'v' => (current.x, --current.y),
+            '>' => (++current.x, current.y),
+            '<' => (--current.x, current.y),
+            _ => throw new ArgumentException()
+        };
+    }
+}
--- AdventOfCode/Aoc-2015/Program.cs
+++ AdventOfCode/Aoc-2015/Program.cs
@@ -1,7 +1,7 @@
 ﻿using AoC;
 using Tools;
 
-var data = await Data.GetInput(2).ToListAsync();
+var data = await Data.GetInput(3).ToListAsync();
 
-Console.WriteLine(Day2.Part1(data));
-Console.WriteLine(Day2.Part2(data));
+Console.WriteLine(Day3.Part1(data.First()));
+Console.WriteLine(Day3.Part2(data.First()));
