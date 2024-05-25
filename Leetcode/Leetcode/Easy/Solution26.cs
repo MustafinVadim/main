0b8db3670710c1ae7d33220c20c93df8c81e4481
Mustafin Vadim <mystafin@skbkontur.ru>
2024-05-25 15:56:11 +0500

    leetcode 26

---
+++ Leetcode/Leetcode/Easy/Solution26.cs
@@ -0,0 +1,16 @@
+﻿namespace Easy;
+
+public class Solution26
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var list = nums.ToHashSet().OrderBy(x => x).ToList();
+
+        for (var i = 0; i < list.Count; i++)
+        {
+            nums[i] = list[i];
+        }
+
+        return list.Count;
+    }
+}
