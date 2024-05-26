9cc12f3779f057cd30da86391b028cdf94a44bc4
Mustafin Vadim <mystafin@skbkontur.ru>
2024-05-26 16:04:01 +0500

    solution 11

---
+++ Leetcode/Leetcode/Easy/Solution11.cs
@@ -0,0 +1,25 @@
+﻿namespace Easy;
+
+public class Solution11
+{
+    public int MaxArea(int[] height) {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int minHeight = Math.Min(height[left], height[right]);
+            int area = width * minHeight;
+            maxArea = Math.Max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
